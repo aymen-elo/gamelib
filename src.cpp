26a346f3ad24c3a3a96dab5b0e867d7c086fe427
Aymen EL OUAGOUTI <aymen.elouagouti@gmail.com>
2023-01-07 23:34:28 +0100

    rename

--- src.cpp
+++ src.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 struct game_t{
     string title;
-    int release_y;
-    double size;
+    int releaseYear;
+    float size;
 };
 
 struct personalGame_t{
-    int playtime_h;
-    double nb_tophies;
+    int playtimeHours;
+    int trophiesNumber;
     game_t* game;
-    bool is_installed;
+    bool isInstalled;
 };
 
 /** a collection is a pointer to an array of pointers **/
 struct collection_t{
-    int nb_games;
-    double max_memory;
+    int gamesNumber;
+    int installedGamesNumber;
+    float maxMemory;
     personalGame_t** collection;
-    int capa;
-    int occup;
-    int nb_installed;
+    int capacity;
+    int occupied;
 };
 
 int main(){
