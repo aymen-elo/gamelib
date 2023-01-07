519f408df44521c43875a153f35c56a6874e29ba
Aymen EL OUAGOUTI <aymen.elouagouti@gmail.com>
2023-01-08 00:44:22 +0100

    add addGame

--- src.cpp
+++ src.cpp
@@ -44,10 +44,41 @@ collection_t createCollection(float maxMemo){
     coll.collection[0]->game = &g_ghostGame;
 
     coll.gamesNumber = 0;
-    coll.installedGamesNumber = 0;
+    coll.installedGamesNumber = 1;
     coll.maxMemory = maxMemo;
 
     return coll;
+}
+
+float occupiedSize(collection_t coll){
+    float size = 0.0;
+    for(int i = 0; i < coll.installedGamesNumber; i++){
+        if(coll.collection[i]->isInstalled)
+            size += coll.collection[i]->game->size;
+    }
+
+    return size;
+}
+
+void addGame(game_t & game, collection_t & coll){
+
+    if((coll.maxMemory - occupiedSize(coll)) < game.size){
+        cout<<"Not enough storage"<<endl;
+        return;
+    }
+
+    personalGame_t** temp = new personalGame_t*[coll.gamesNumber + 1];
+
+    /**i=1 because we have the ghostGame in the beggining**/
+    for(int i = 1; i < coll.installedGamesNumber; i++){
+        temp[i] = coll.collection[i];
+    }
+    
+    *temp[coll.installedGamesNumber] = {0, 0, &game, false};
+    
+    for(int i = coll.installedGamesNumber+1; i < coll.gamesNumber; i++){
+        temp[i] = coll.collection[i];
+    }
 }
 
 void desallocation(collection_t coll){
